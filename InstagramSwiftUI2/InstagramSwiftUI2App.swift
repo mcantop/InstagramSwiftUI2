e866b53558209100b7d23cb670b3d73a8bf3957d
mcantop <maciejcantop@icloud.com>
2023-05-13 03:28:45 +0200

    Initial Commit

---
+++ InstagramSwiftUI2/InstagramSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUI2App.swift
+//  InstagramSwiftUI2
+//
+//  Created by Maciej on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
