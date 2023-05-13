534a6f78eb6ed9fd65fd2ebac99a52ddb6330c38
mcantop <maciejcantop@icloud.com>
2023-05-13 04:56:06 +0200

    FeedView + FeedCell UI

---
+++ InstagramSwiftUI2/Core/Feed/FeedCell.swift
@@ -0,0 +1,70 @@
+//
+//  FeedCell.swift
+//  InstagramSwiftUI2
+//
+//  Created by Maciej on 13/05/2023.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            HStack {
+                Image("profile_pic")
+                    .resizable()
+                    .frame(width: 44, height: 44)
+                    .clipShape(Circle())
+                
+                Text("future").bold()
+                
+                Spacer()
+            }
+            .padding(.horizontal, 8)
+            
+            Image("post_pic")
+                .resizable()
+                .frame(maxWidth: .infinity)
+                .scaledToFit()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(spacing: 16) {
+                    FeedCellActionButton(imageName: "heart") { }
+                    FeedCellActionButton(imageName: "message") { }
+                    FeedCellActionButton(imageName: "paperplane") { }
+                    
+                    Spacer(minLength: 0)
+                }
+                
+                Text("0 likes").bold()
+                
+                Text("future").bold() + Text(" ") + Text("Winning without u.")
+                
+                Text("6h")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.horizontal, 8)
+        }
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
+
+struct FeedCellActionButton: View {
+    let imageName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: imageName)
+                .font(.title)
+        }
+    }
+}
---
+++ InstagramSwiftUI2/Core/Feed/FeedView.swift
@@ -0,0 +1,54 @@
+//
+//  FeedView.swift
+//  InstagramSwiftUI2
+//
+//  Created by Maciej on 13/05/2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0..<5, id: \.self) { _ in
+                        FeedCell()
+                    }
+                }
+                .padding(.top)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image("instagram-wide-logo")
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(.primary)
+                            .frame(width: 100, height: 60)
+                    }
+
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "paperplane")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
